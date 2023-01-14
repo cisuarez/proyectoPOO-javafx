da895c5a5a306600a895e78f69fb853eabbc491a
DanielV <danlevil@espol.edu.ec>
2023-01-14 15:24:40 -0500

    Controlador CrearPermisos, verificacion de espacios vacios. Falta que los tipos de datos sean correctos.

--- src/main/java/danlevil/proyctpoofxmlgluon/CrearPermisoController.java
+++ src/main/java/danlevil/proyctpoofxmlgluon/CrearPermisoController.java
@@ -13,9 +13,12 @@ import Modelo.Visitante;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -26,34 +29,39 @@ import javafx.scene.control.TextField;
  * @author DANIEL
  */
 public class CrearPermisoController implements Initializable{
-    @FXML
-    private TextField cedulaResidente;
+    @FXML private TextField cedulaResidente;
     @FXML private Label fechaActual;
-    @FXML
-    private DatePicker fechaIngreso;
-    @FXML
-    private TextField horaEntrada ;
+    @FXML private DatePicker fechaIngreso;
+    @FXML private TextField horaEntrada ;
     
-    @FXML
-    private TextField cedulaVisitante;
-    @FXML
-    private TextField duracionVisita;
+    @FXML private TextField cedulaVisitante;
+    @FXML private TextField duracionVisita;
     
-    @FXML
-    private Button crearPermiso;
-    @FXML
-    private Button salirCrearPermiso;
-    @FXML
-    private Label infoPermisoCreado;
+    @FXML private Button crearPermiso;
+    @FXML private Button salirCrearPermiso;
+    @FXML private Label infoPermisoCreado;
+    
+    private ArrayList <String> errores;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         LocalDate lD= LocalDate.now();
         fechaActual.setText(lD.toString());
+        errores=new ArrayList<>();
+        
     }
     
     @FXML
     private void crear(){
+        verificar();
+        if(errores.size()>0){
+            Alert espaciosVacios= new Alert(AlertType.ERROR);
+            espaciosVacios.setTitle("ERROR. Espacios sin completar");
+            espaciosVacios.setContentText("TIENE QUE LLENAR TODOS LOS CAMPOS");
+            espaciosVacios.initOwner(App.ventanaPrincipal);
+            espaciosVacios.show();
+            return;
+        }
         Residente res=new Residente();
         Visitante vis=new Visitante();
         
@@ -61,6 +69,7 @@ public class CrearPermisoController implements Initializable{
         if(!cedulaResidente.getText().isEmpty() ){
             if(App.urb.encontrarPersona(cedulaResidente.getText())instanceof Residente) {
                 res=(Residente)App.urb.encontrarPersona(cedulaResidente.getText());
+                
             }
             else{res=null;}
         }
@@ -78,6 +87,7 @@ public class CrearPermisoController implements Initializable{
                 Permiso.definirHora(duracionVisita.getText())     
         
         );
+      
         System.out.println("se creo el permiso");
         infoPermisoCreado.setText(permisoNuevo.toString());
         res.getPermisos().add(permisoNuevo);
@@ -94,7 +104,30 @@ public class CrearPermisoController implements Initializable{
         System.out.println(mes.getDayOfMonth());*/
     }
     
-    
+    private void verificar(){
+        errores.clear();
+        if(cedulaResidente.getText().isEmpty()){
+            errores.add("Cedula del residente no ingresada");
+            
+        }
+        if(cedulaVisitante.getText().isEmpty()){
+            errores.add("Cedula del Visitante no ingresada");
+            
+        }
+        if(horaEntrada.getText().isEmpty()){
+            errores.add("hora de entrada  no ingresada");
+            
+        }
+        if(duracionVisita.getText().isEmpty()){
+            errores.add("Duración de la visita no ingresada");
+            
+        }
+        if(fechaIngreso.getValue()==null){
+            errores.add("Fecha de ingreso no seleccionada");
+            
+        }
+
+    }
     
     
     
